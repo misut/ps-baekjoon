d9801dd3f5ba52113660ac5e3503e0905e46931d
misut <pkr5207@gmail.com>
2020-02-29 00:04:18 +0900

    Study: 2020-02-29

---
+++ 1920.py
@@ -0,0 +1,10 @@
+if __name__ == '__main__':
+    N = int(input())
+    A = {int(x) : True for x in input().strip().split()}
+    M = int(input())
+    B = [int(x) for x in input().strip().split()]
+    for n in B:
+        if n in A:
+            print(1)
+        else:
+            print(0)
