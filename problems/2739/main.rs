cfdbc8d1f6095ad56f42cb6fa625903a458d1b6c
misut <pkr5207@gmail.com>
2022-06-12 17:32:22 +0900

    study: solved 2739 with Rust

---
+++ problems/2739/main.rs
@@ -0,0 +1,15 @@
+use std::io;
+
+fn main() {
+    let mut number = String::new();
+
+    io::stdin()
+        .read_line(&mut number)
+        .expect("Failed to read line");
+
+    let number: i32 = number.trim().parse().expect("Cannot parse number");
+
+    for multiplier in 1..10 {
+        println!("{} * {} = {}", number, multiplier, number * multiplier);
+    }
+}
